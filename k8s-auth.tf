8c416dc9b97cf7a282e3faae1cee86437e66dc78
craspoo <camilarspoo@gmail.com>
2025-10-05 17:55:16 -0300

    c1

--- k8s-auth.tf
+++ k8s-auth.tf
@@ -1,5 +1,6 @@
 resource "kubectl_manifest" "aws_auth" {
   depends_on = [aws_eks_cluster.cluster]
+
   yaml_body = <<YAML
 apiVersion: v1
 kind: ConfigMap
@@ -7,9 +8,9 @@ metadata:
   name: aws-auth
   namespace: kube-system
 data:
-  mapUsers: |
-    - userarn: arn:aws:iam::579375260812:user/camila
-      username: camila
+  mapRoles: |
+    - rolearn: arn:aws:iam::579375260812:role/GitHubActionRole-DatabaseInfra
+      username: github-actions
       groups:
         - system:masters
 YAML
