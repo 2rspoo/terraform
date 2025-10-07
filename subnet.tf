7889b16036374a15fe3b82c054b9423147a09bb2
2rspoo <rodrigo.spoo@gmail.com>
2025-10-07 00:01:58 -0300

    teste

--- subnet.tf
+++ subnet.tf
@@ -5,16 +5,15 @@ resource "aws_subnet" "subnet_public" {
   map_public_ip_on_launch = true
   availability_zone       = ["us-east-1a", "us-east-1b", "us-east-1c"][count.index]
 
-  # --- MUDANÇA APLICADA AQUI ---
+  # --- CORREÇÃO APLICADA AQUI ---
   # A função merge() une o mapa de tags da sua variável com o novo mapa de tags do EKS.
   tags = merge(
     var.tags,
     {
-      # Tag para o EKS reconhecer a subnet (OBRIGATÓRIA)
       "kubernetes.io/cluster/eks-spoo-ent9-backend" = "shared",
-
-      # Tag para permitir a criação de Load Balancers (BOA PRÁTICA)
       "kubernetes.io/role/elb"                      = "1"
     }
   )
+}
+
 }
